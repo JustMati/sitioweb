438ec2f568fc1de90218660c44de096bc0f3ef1b
mathew <matthewelnoob@gmail.com>
2021-11-16 20:26:21 -0500

    actua

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,17 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 });
+/* Modal */
+const modalAdd = document.querySelector('#modalAdd');
+
+const openModal = () => {
+	modalAdd.style.display = 'flex';
+}
+const closeModal = () => {
+	modalAdd.style.display = 'none';
+}
+modalAdd.onclick = (event) => {
+	if(event.target == modalAdd){
+		closeModal();
+	}
+}
